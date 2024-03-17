402808ee5f553fbb5eeb73d0aa0367c9d8838e07
nicolasgarza <nicolas.garza@gmail.com>
2024-03-16 22:54:43 -0500

    init

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Message struct {
+	Topic   string
+	Payload interface{}
+}
+
+type Subscriber struct {
+	Channel     chan interface{}
+	Unsubscribe chan bool
+}
+
+type Broker struct {
+	subscribers map[string][]*Subscriber
+	mutex       sync.Mutex
+}
+
+func NewBroker() *Broker {
+	return &Broker{
+		subscribers: make(map[string][]*Subscriber),
+	}
+}
+
+func main() {
+	fmt.Print("HELLO")
+	time.Sleep(0)
+}
