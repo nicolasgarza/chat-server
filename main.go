012b6cabffb6040ca43a8c1a448497356fe5fa71
Nicolas Garza <nicolas.garza@gmail.com>
2024-03-18 14:32:42 -0500

    basic message broker done

--- main.go
+++ main.go
@@ -27,7 +27,78 @@ func NewBroker() *Broker {
 	}
 }
 
+func (b *Broker) Subscribe(topic string) *Subscriber {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	subscriber := &Subscriber{
+		Channel:     make(chan interface{}, 1),
+		Unsubscribe: make(chan bool),
+	}
+
+	b.subscribers[topic] = append(b.subscribers[topic], subscriber)
+
+	return subscriber
+}
+
+func (b *Broker) Unsubscribe(topic string, subscriber *Subscriber) {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	if subscribers, found := b.subscribers[topic]; found {
+		for i, sub := range subscribers {
+			if sub == subscriber {
+				close(sub.Channel)
+				b.subscribers[topic] = append(subscribers[:i], subscribers[i+1:]...)
+				return
+			}
+		}
+	}
+}
+
+func (b *Broker) Publish(topic string, payload interface{}) {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	if subscribers, found := b.subscribers[topic]; found {
+		for _, sub := range subscribers {
+			select {
+			case sub.Channel <- payload:
+			case <-time.After(time.Second):
+				fmt.Printf("Subscriber slow. Unsubscribing from topic: %s\n", topic)
+				b.Unsubscribe(topic, sub)
+			}
+		}
+	}
+}
+
 func main() {
-	fmt.Print("HELLO")
-	time.Sleep(0)
+	broker := NewBroker()
+
+	subscriber := broker.Subscribe("example_topic")
+	go func() {
+		for {
+			select {
+			case msg, ok := <-subscriber.Channel:
+				if !ok {
+					fmt.Println("Subscriber channel closed.")
+					return
+				}
+				fmt.Printf("Recieved: %v\n", msg)
+			case <-subscriber.Unsubscribe:
+				fmt.Println("Unsubscribed.")
+				return
+			}
+		}
+	}()
+
+	broker.Publish("example_topic", "Hello, World!")
+	broker.Publish("example_topic", "This is a test message")
+
+	time.Sleep(2 * time.Second)
+	broker.Unsubscribe("example_topic", subscriber)
+
+	broker.Publish("example_topic", "This message won't be recieved.")
+
+	time.Sleep(time.Second)
 }
